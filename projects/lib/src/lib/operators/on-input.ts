36e2cf461888dbb152a6d786b3c801f190f6c780
Philipp Burgmer <burgmer@pdtec.de>
2020-01-31 14:05:44 +0100

    add currentValueOf operator

--- projects/lib/src/lib/operators/on-input.ts
+++ projects/lib/src/lib/operators/on-input.ts
@@ -2,6 +2,11 @@ import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { TypedChange, TypedChanges } from '../hooks/on-changes';
 
+/**
+ * Observable operator for easier usage of {@see OnChanges$}. Only emits if the given property has changed.
+ *
+ * @param property
+ */
 export function onInput<T, P extends keyof T>(property: P): (source: Observable<TypedChanges<T>>) => Observable<TypedChange<T[P]>> {
   return (source: Observable<TypedChanges<T>>) => {
     return source
