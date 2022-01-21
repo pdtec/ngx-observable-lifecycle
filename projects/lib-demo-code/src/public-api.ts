e28145f71dfbd99a318d3b1fa63379065166416e
Philipp Burgmer <burgmer@pdtec.de>
2022-01-21 12:27:46 +0100

    fix public api

--- projects/lib-demo-code/src/public-api.ts
+++ projects/lib-demo-code/src/public-api.ts
@@ -2,11 +2,12 @@
  * Public API Surface of lib-demo-code
  */
 
-export { ObservableService } from './lib/observable.service';
-export { LibDemoCodeModule } from './lib/lib-demo-code.module';
-export { AppComponent } from './lib/app.component';
-export { Child1Component } from './lib/child-1.component';
-export { Child2Component } from './lib/child-2.component';
-export { OnChangesComponent } from './lib/on-changes.component';
-export { SubscriberCounterComponent } from './lib/subscriber-counter.component';
+export * from './lib/observable.service';
+export * from './lib/lib-demo-code.module';
+export * from './lib/app.component';
+export * from './lib/child-1.component';
+export * from './lib/child-2.component';
+export * from './lib/on-changes.component';
+export * from './lib/subscriber-counter.component';
+export * from './lib/usage';
 
