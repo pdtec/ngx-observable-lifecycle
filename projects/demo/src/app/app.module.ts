f80381fe8c35a30c51a3751b9fe694e44ae8a878
Philipp Burgmer <burgmer@pdtec.de>
2022-01-21 15:45:38 +0100

    inline demo-code and test-code libs

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent, LibDemoCodeModule } from 'lib-demo-code';
+import { Child1Component } from './child-1.component';
+import { Child2Component } from './child-2.component';
+import { OnChangesComponent } from './on-changes.component';
+import { SubscriberCounterComponent } from './subscriber-counter.component';
+import { UsageComponent, UsageDirective, UsagePipe } from './usage';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    Child1Component,
+    Child2Component,
+    OnChangesComponent,
+    SubscriberCounterComponent,
+    UsageDirective,
+    UsageComponent,
+    UsagePipe,
   ],
   imports: [
     BrowserModule,
-    LibDemoCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
