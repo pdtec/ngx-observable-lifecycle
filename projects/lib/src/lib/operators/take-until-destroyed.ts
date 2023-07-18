ade4daee5921d1a731f8a72ba124539c58cc5da5
Philipp Burgmer <burgmer@pdtec.de>
2023-07-18 16:54:32 +0200

    feat: deprecate takeUntilDestroyed in favor of Angular's own implementation

--- projects/lib/src/lib/operators/take-until-destroyed.ts
+++ projects/lib/src/lib/operators/take-until-destroyed.ts
@@ -2,6 +2,9 @@ import { Observable } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { IOnDestroy$ } from '../hooks/on-destroy';
 
+/**
+ * @deprecated: use Angular's takeUntilDestroyed which works without inheritance
+ */
 export function takeUntilDestroyed<T>(x: IOnDestroy$): (source: Observable<T>) => Observable<T> {
   return (source: Observable<T>) => {
     return source
